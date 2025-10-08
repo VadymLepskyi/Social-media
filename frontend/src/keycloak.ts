ce84c7c0f4d39581a81d2b9f9836ab3b64fa95d4
VadymLepskyi <144442473+VadymLepskyi@users.noreply.github.com>
2025-10-08 15:34:45 +0200

    Registration has been set up

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
   url: "http://localhost:8080",
   realm: "MyProjectRealm",
-  clientId: "frontend-client",
+  clientId: "myclient",
 });
 
 export default keycloak;
